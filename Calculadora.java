7a99adbbbda5c9c93d13f12cfea4fef94740da83
Felipe Furtado <felipeffurtado.eng@gmail.com>
2020-02-23 13:51:18 -0300

    Criado menu e add func de Multiplicação

--- Calculadora.java
+++ Calculadora.java
@@ -1,16 +1,53 @@
 import java.util.Scanner;
     
     public class Calculadora{
+
+        public int som(int num1, int num2){
+            return num1 + num2;
+        }
+        public int sub(int num1, int num2){
+            return num1 - num2;
+        }
         public static void main(String [] args){
+            Calculadora c = new Calculadora();
+
+            int opcao = 0;
             int num1;
             int num2;
 
-            Scanner input = new Scanner(System.in);
+                Scanner input = new Scanner(System.in);
 
             System.out.println("Digite o numero 1:");
             num1 = input.nextInt();
             System.out.println("Digite o numero 2:");
             num2 = input.nextInt();
-            System.out.println("Resultado: " + (num1 + num2));
+
+                Scanner inputOp = new Scanner(System.in);
+
+            System.out.println("O que faremos com eles?");
+            System.out.println("1. Soma");
+            System.out.println("2. Subtracao");
+            System.out.println("0. Sair");
+
+            opcao = inputOp.nextInt();
+
+            while (opcao != 0){
+
+                if (opcao == 1){
+
+                    int conta = c.som(num1, num2);
+
+                    System.out.println("Igual a " + conta);
+                    break;
+                }
+                if (opcao == 2){
+                    
+                    int conta = c.sub(num1, num2);
+
+                    System.out.println("Igual a " + conta);
+                    break;
+                }
+
+            }
         }
     }
